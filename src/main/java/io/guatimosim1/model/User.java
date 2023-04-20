7741ec2fb8fe26ed065a3488fe34c81738167c9c
Darlan Maia <darlanmaia2010@hotmail.com>
2023-04-20 11:12:50 -0300

    (main) Commit Inicial

---
+++ src/main/java/io/guatimosim1/model/User.java
@@ -0,0 +1,21 @@
+package io.guatimosim1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "TB_USER")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String lastname;
+}
